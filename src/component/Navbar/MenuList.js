fd5e8195ca14716395d0b6bd44a7cbe6faaffa99
sagara krishantha <sagarakrishantha96@gmail.com>
2023-02-02 22:01:09 +0530

    first commit

---
+++ src/component/Navbar/MenuList.js
@@ -0,0 +1,23 @@
+export const MenuList=[
+    {
+        title:"Home",
+        url: "/"
+    },
+    {
+        title:"KeyFeatures",
+        url: "/features"
+    },
+    {
+        title:"Pricing",
+        url: "/pricing"
+    },
+    {
+        title:"Testimonials",
+        url: "/testimonials"
+    },
+    {
+        title:"Try for Free",
+        url: "/demo"
+    }
+
+];
---
+++ src/component/pages/KeyFeatures.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const KeyFetures = () => {
+    return ( 
+     <div className="page-heading">
+             <h1>KeyFetures</h1>
+        </div>
+     );
+}
+ 
+export default  KeyFetures ;
